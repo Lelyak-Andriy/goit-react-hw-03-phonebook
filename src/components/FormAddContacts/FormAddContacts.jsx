2979c308b35e47c2df51728d22d1a0308792a0f5
Lelyak-Andriy <a.lelyak47@gmail.com>
2022-02-21 19:42:12 +0200

    validation added

--- src/components/FormAddContacts/FormAddContacts.jsx
+++ src/components/FormAddContacts/FormAddContacts.jsx
@@ -44,6 +44,10 @@ class FormAddContacts extends Component {
           Name
           <input
             type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
             placeholder="Name Surname"
             value={name}
             onChange={(e) => this.handleChangeName(e.target.value)}
@@ -53,6 +57,10 @@ class FormAddContacts extends Component {
           Number
           <input
             type="number"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
             placeholder="Phone"
             value={number}
             onChange={(e) => this.handleChangeNumber(e.target.value)}
